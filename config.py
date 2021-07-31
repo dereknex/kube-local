5e73d51d6c016d2b1ec85e56bf904a512e251249
Derek Chen <derek_chen@live.com>
2021-08-01 00:15:47 +0800

    install pyyaml

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
-class Configuration:
+import yaml
+
+class Configuration(yaml.YAMLObject):
+
+    
     auto_clean = True
     local_path = 'temp/'
+    inputs = []
+    outputs = []
+    tasks = []
     
+    def __init__(self, yaml_data):
+        d = yaml.loader.construct_mapping(
+        
