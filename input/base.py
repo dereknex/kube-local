3427c22cda2a298c855d944b371ed36915b5d5f3
Derek Chen <derek_chen@live.com>
2021-08-05 18:13:41 +0800

    add informer

--- input/base.py
+++ input/base.py
@@ -1,59 +1,6 @@
-from enum import Enum
-
-class Status(Enum):
-    ERROR = 0b00
-    WATING = 0b01
-    IN_PROGRESS = 0b10
-    DONE = 0b11
-class Input:
+from informer import Informer
+class Input(Informer):
     name = ""
-
-    _status:Status = Status.WATING
-    _progress = 0
-
-    _observers = []
-
-    @property
-    def status(self):
-        return self._status
-
-    @status.setter
-    def status(self, value):
-        if value == Status.DONE:
-            self._progress = 100
-        elif value == Status.ERROR or value == Status.WATING:
-            self._progress = 0
-        if self._status != value:
-            self._status = value
-            self.notify()
-
-    @property
-    def progress(self):
-        return self._progress
-
-    @progress.setter
-    def progress(self, value):
-        if value <0:
-            value  = 0
-        if value >100 :
-            value = 100
-        if value == 100:
-            self.status = Status.DONE
-        if value >0 and value < 100:
-            self.status = Status.IN_PROGRESS
-        if value ==0:
-            self.status = Status.WATING
-        if value != self._progress:
-            self._progress = value
-            self.notify()
-
-    def watch(self, observer):
-        if observer not in self._observers:
-            self._observers.append(observer)
-    
-    def notify(self):
-        for observer in self._observers:
-            observer.update_progress(self)
 
     def download(self) -> None:
         raise NotImplementedError()
--- output/base.py
+++ output/base.py
@@ -1,3 +1,6 @@
-class Output:
+from informer import Informer
+
+
+class Output(Informer):
     def upload(self) -> None:
         raise NotImplementedError()
